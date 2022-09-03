b0913f98272efd96b4b8a076f45b907463a884fd
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-02 21:09:56 -0700

    🚧 Add other template

--- src/nies-init.py
+++ src/nies-init.py
@@ -11,7 +11,21 @@ env = Environment(loader=fileLoader)
 
 @app.command()
 def dc(repo=""):
+    """
+    docker-compose
+    """
     rendered = env.get_template("docker-compose.yml").render(
+        repo=repo,
+    )
+    print(rendered)
+
+
+@app.command()
+def am(repo=""):
+    """
+    actions main
+    """
+    rendered = env.get_template("main.yml").render(
         repo=repo,
     )
     print(rendered)

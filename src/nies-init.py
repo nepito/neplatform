477c4fb997b60b20c8cd020bd6f3031f95dbad3f
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-02 21:23:15 -0700

    🚧 Update template

--- src/nies-init.py
+++ src/nies-init.py
@@ -8,6 +8,7 @@ app = typer.Typer()
 fileLoader = FileSystemLoader("neplatform/templates/")
 env = Environment(loader=fileLoader)
 
+TAG ={"main": "stable", "develop": "latest"}
 
 @app.command()
 def dc(repo=""):
@@ -21,12 +22,14 @@ def dc(repo=""):
 
 
 @app.command()
-def am(repo=""):
+def am(repo="", branch="develop"):
     """
     actions main
     """
     rendered = env.get_template("main.yml").render(
         repo=repo,
+	branch=branch,
+	tag=TAG[branch]
     )
     print(rendered)
 
